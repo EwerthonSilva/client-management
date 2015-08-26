357baa42d9aad62a063d99c752ceaac89a973ff3
EwerthonSilva <ewerthon.silva@s2it.colm.br>
2015-08-25 21:27:25 -0300

    Create a new project to client management

---
+++ src/main/java/br/com/incubadora/controller/ClientController.java
@@ -0,0 +1,6 @@
+package br.com.incubadora.controller;
+
+
+public class ClientController {
+
+}
---
+++ src/main/java/br/com/incubadora/model/Client.java
@@ -0,0 +1,72 @@
+package br.com.incubadora.model;
+
+import java.util.Date;
+
+public class Client {
+
+	// Builder default
+	public Client() {
+
+	}
+
+	// Builder to many methods
+	public Client(int id) {
+		this.id = id;
+	}
+
+	private int id;
+	private String name;
+	private String surname;
+	private Date birthday;
+	private String cpf;
+	private String rg;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+}
